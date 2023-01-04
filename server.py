fa081250faf09eef80f9bbd8a9af7e6163bc8164
Brendan Cordova <cordovalegacy19@gmail.com>
2023-01-04 13:29:38 -0800

    Fully Functional Dynamic Checkerboard

--- server.py
+++ server.py
@@ -5,5 +5,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', num_1 = 8, num_2 = 8)
 
+@app.route('/<int:num_1>')
+def row(num_1):
+    return render_template('index.html', num_1 = num_1, num_2 = 8)
+
+@app.route('/<int:num_1>/<int:num_2>')
+def row_and_col(num_1, num_2):
+    return render_template('index.html', num_1 = num_1, num_2 = num_2)
+
+@app.route('/<int:num_1>/<int:num_2>/<color_1>')
+def board_color_1(num_1, num_2, color_1):
+    return render_template('index.html', num_1 = num_1, num_2 = num_2, color_1 = color_1)
+
+@app.route('/<int:num_1>/<int:num_2>/<color_1>/<color_2>')
+def board_color_2(num_1, num_2, color_1, color_2):
+    return render_template('index.html', num_1 = num_1, num_2 = num_2, color_1 = color_1, color_2 = color_2)
+
 if __name__=='__main__':
     app.run(debug=True)
